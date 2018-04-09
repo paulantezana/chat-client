5f87ebcc90bb4094735be4a3587a4ae99340b084
paul antezana <contacto@paulantezana.com>
2018-04-09 14:03:36 -0500

    new updates

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const ExtractTextPlugin     = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin     = require('html-webpack-plugin');
+const path                  = require('path');
+
+module.exports = {
+    entry:{
+        index        : './bin/index.jsx',
+    },
+
+    output: {
+        path        : path.resolve(__dirname,'public'),
+        filename    : 'js/[name].js',
+    },
+
+    devServer: {
+        contentBase : path.join(__dirname, "public"),
+        compress    : true,
+        port        : 3005,
+        open        : true,
+        stats       : 'errors-only',
+    },
+
+    module: {
+        rules:[
+            {
+                test    : /\.scss/,
+                use     : ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use : ['css-loader','postcss-loader','sass-loader']
+                })
+                // use: ['style-loader','css-loader','postcss-loader','sass-loader']
+            },
+
+            {
+                test    : /\.(jsx?|js)$/,
+                exclude : /node_modules/,
+                use     : 'babel-loader'
+            },
+
+            {
+                test    : /\.json$/,
+                use     : 'json-loader'
+            },
+
+            {
+                test: /\.md$/,
+                use: ['react-markdown-loader']
+            },
+
+            {
+                test    : /\.pug$/,
+                use     : ['html-loader','pug-html-loader']
+            },
+
+            {
+                test    : /\.(png|jpg|svg)$/,
+                use     : 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename    : 'index.html',
+            minify      : {
+                collapseWhitespace: false
+            },
+            hash        : true,
+            template    : './bin/index.pug',
+        }),
+
+        new ExtractTextPlugin({
+            filename:  (getPath) => {
+                return getPath('css/[name].css').replace('css/js', 'css');
+            },
+            allChunks: false
+        }),
+    ]
+}
