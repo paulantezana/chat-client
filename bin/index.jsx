d3acbfd1983a7a477e7b0518ad22fd386def4716
paul antezana <contacto@paulantezana.com>
2018-04-13 09:27:30 -0500

    new updates2

--- bin/index.jsx
+++ bin/index.jsx
@@ -5,6 +5,42 @@ import ReactDOM from 'react-dom';
 
 // style libraries
 
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+fetch("http://localhost:7070/login",{
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        username: "anita",
+        pass:"123"
+    })
+})
+.then((Response) => {
+    console.log(Response.json())
+})
+.then((data) =>  console.log(data))
+.catch((err)=>console.log(err));
+
+
+const APYKEY = "secret";
+
+const getConnection = () => {
+    const ts = Date.now;
+    ts
+}
+
+
+
+
+
+
+
+
+
 import App from "./components/index.jsx"
 
 ReactDOM.render(
