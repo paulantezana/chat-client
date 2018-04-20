35176f2ef2403206bfb949b460d91f83cd52c6e7
paul antezana <contacto@paulantezana.com>
2018-04-20 16:03:42 -0500

    new updates

--- bin/components/pages/login.jsx
+++ bin/components/pages/login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Checkbox, Form, Input, Label } from 'semantic-ui-react';
+import { Button, Form, Icon, Input, Divider, Message } from 'semantic-ui-react';
 import login from "./login.scss";
 import { runInThisContext } from "vm";
 
@@ -7,29 +7,64 @@ class Login extends Component{
 
     constructor(props){
         super(props);
+        this.state = {
+            args: {}
+        }
+
         this.onSubmit = this.onSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e, input){
+        const args = this.state.args;
+        args[input.name] = input.value;
+        this.setState({args});
     }
 
     onSubmit(e){
         e.preventDefault();
     }
+
+
     render(){
         return(
-            <div className="login-container">
+            <div className="block block-login">
+                <h1 className="text-center">LOGIN</h1>
+
+                <Message negative>
+                    <Message.Header>Error</Message.Header>
+                    <p>Nombre de usuario o contraseña incorrecta</p>
+                </Message>
+
                 <Form onSubmit={this.onSubmit}>
+
                     <Form.Field>
-                        <Input icon='user' iconPosition='left' placeholder='Nombre de usuario' />
+                        <Input
+                            placeholder='Nombre de usuario'
+                            name="username"
+                            onChange={this.handleChange}
+                            icon={<Icon name='close'/>}  />
                     </Form.Field>
+
                     <Form.Field>
-                        <Input icon='user' iconPosition='left' placeholder='Contraseña' type="password" />
+                        <Input
+                            placeholder='Contraseña'
+                            type="password"
+                            name="password"
+                            onChange={this.handleChange}
+                            icon={<Icon color='green' name='check'/>} />
                     </Form.Field>
-                    <Button.Group>
-                        <Button type="reset">Cancelar</Button>
-                        <Button.Or text='O'/>
-                        <Button type='submit' color="positive">enviar</Button>
-                    </Button.Group>
+
+                    <Button
+                        disabled={!this.state.args.username || !this.state.args.password}
+                        type='submit' color='primary' fluid>Ingresar</Button>
+
                 </Form>
-                <a href="">olvido su contraseña</a>
+
+                <Divider horizontal>O</Divider>
+
+                <a href="" className="text-center">olvido su contraseña</a>
+
             </div>
         )
     }
