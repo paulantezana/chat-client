7be4594850170ae88db517bf4663b8450db53345
paul antezana <contacto@paulantezana.com>
2018-05-18 09:21:08 -0500

    new updates

--- bin/components/pages/init.jsx
+++ bin/components/pages/init.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import "./init.scss";
+import IMGStart from "./../media/start.jpg";
 
 import { Link } from "react-router-dom";
-const Init = ()=>{
-    return <div>
-                <h1>App</h1>
-                <Link to="/app">Applicacion</Link>
-            </div>
-}
-
+const Init = ()=>(
+    <div className="welcome">
+        <div className="welcome__img">
+            <img src={IMGStart} alt=""/>
+        </div>
+        <div className="welcome__content">
+            <h1>Bienvenido a la revolución</h1>
+            <Link to="/app" className="start-app">Ingresar</Link>
+        </div>
+    </div>
+);
 export default Init;
